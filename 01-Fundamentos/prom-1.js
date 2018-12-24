08922bd9b14c00ed04a1a1b0879e129d0a5201f9
gescobar <gescobartri@gmail.com>
2018-12-23 21:52:49 -0600

    prom 1

---
+++ 01-Fundamentos/prom-1.js
@@ -0,0 +1,40 @@
+function sumarUno( numero, callback) {
+
+    if (numero >= 7) {
+        callback('Número muy alto');
+        return;
+    }
+    setTimeout( function() {
+
+        callback( null, numero + 1 );
+
+    }, 800);
+
+}
+
+sumarUno (5, function( error, nuevoValor ) {
+        
+if ( error ) {
+    console.log( error );
+    return;
+}
+    sumarUno( nuevoValor, function ( error, nuevoValor2 ) {
+        
+if ( error ) {
+    console.log( error );
+    return;
+}
+        sumarUno( nuevoValor2, function ( error, nuevoValor3 ) {
+
+if ( error ) {
+    console.log( error );
+    return;
+}
+            console.log( nuevoValor3 );
+            
+            
+        });
+
+        
+    });
+});
