a167b764dc7fa08bc074c60bae7ce2b5260b9725
gescobar <gescobartri@gmail.com>
2019-01-03 18:03:46 -0600

    samples cache's methods

--- 04-cache-offline/js/app.js
+++ 04-cache-offline/js/app.js
@@ -3,3 +3,35 @@
 if ( navigator.serviceWorker ) {
     navigator.serviceWorker.register('/sw.js');
 }
+
+if( window.caches ) {
+
+    caches.open('prueba-1');
+
+    caches.has('prueba-2').then( console.log );
+
+    caches.delete('prueba-1').then( console.log );
+
+    caches.open('cache-v1.1').then( cache => {
+
+        //cache.add('/index.html');
+        cache.addAll([
+            '/index.html',
+            '/css/style.css',
+            '/img/main.jpg'
+        ]).then( () => {
+
+            cache.delete('/css/style.css');
+
+        });
+
+        cache.match('/index.html') 
+            .then( resp => {
+                resp.text().then( console.log );
+            });
+    });
+
+    caches.keys().then( keys => {
+        console.log( keys );
+    })
+}
